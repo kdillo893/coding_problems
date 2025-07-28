07c05f3fde11d02320990b9e1097d66c94a0bfb6
Kevin Dillon <kdillo893@gmail.com>
2025-07-27 19:18:26 -0500

    huffman minor change

---
+++ algorithms/huffman.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  // making a "table" of chars and the counts of those chars:
+  // no need for map, as the char values should lie within the 8bit set of char
+  // null is not counted in this listing, just other chars
+
+  unsigned int charCounts[256] = {0};
+
+  // get the file/value to read the characters, increment by count
+
+  // done counting, make a min heap of characters from this array, excluding any
+  // 0's.
+
+  // now that there's a heap, for the number of characters in the heap, organize
+  // code depending on the character count.
+
+  return EXIT_SUCCESS;
+}
